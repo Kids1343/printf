f18e04ad46f8b5b059a9f7eecfeac4e5804b4d17
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2021-07-27 13:59:39 +0300

    Update holberton.h

--- holberton.h
+++ holberton.h
@@ -30,4 +30,27 @@ char *indentifier;
 int (*printer)(va_list);
 } identifierStruct;
 
+typedef struct printer
+{
+	char format;
+	int (*func)(va_list *);
+} printer_t;
+
+int _putchar(char c);
+printer_t select_printer(char s);
+int _printf(const char *format, ...);
+
+int print_char(va_list *);
+int print_string(va_list *);
+int print_int(va_list *);
+int print_unsigned_int(va_list *);
+int print_double(va_list *);
+int print_octal(va_list *);
+int print_hexa(va_list *);
+int print_mini_hexa(va_list *);
+int print_binary(va_list *);
+int print_special(va_list *);
+int print_rot13(va_list *params);
+int print_reverse(va_list *);
+
 #endif
