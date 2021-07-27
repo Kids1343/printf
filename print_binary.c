112eba4521b7cf769f16772c0611683f3cb8c21b
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2021-07-27 13:47:34 +0300

    Update print_binary.c

--- print_binary.c
+++ print_binary.c
@@ -1,30 +1,16 @@
-#include <stdarg.h>
 #include "holberton.h"
-/**
- * print_base2 - prints numbers in binary recursively
- * @value: The number to be printed
- * @length: The amount of digits to be printed
- *
- * Return: On success length.
- */
-int print_base2(unsigned int value, int length)
-{
-	if (value / 2)
-		length = print_base2(value / 2, length + 1);
-	_putchar(value % 2 + '0');
-	return (length);
-}
-/**
- * print_binary - writes in binary
- * @params: The name for va_list
- *
- * Return: On success length.
- */
-int print_binary(va_list *params)
-{
-	int length = 0, negative = 0;
-	unsigned int b = va_arg(*params, unsigned int);
 
-	length = print_base2(b, length) + negative + 1;
-	return (length);
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number to be printed in binary
+ * @printed: hold the number of characters printed
+ */
+void print_binary(unsigned int n, unsigned int *printed)
+{
+	if (n > 1)
+	{
+		*printed += 1;
+		print_binary(n >> 1, printed);
+	}
+	_putchar((n & 1) + '0');
 }
